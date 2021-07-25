23d3c803e1f83c4db078379333d4b8506f1b3aa0
Austin Totty <awtotty@gmail.com>
2021-07-25 15:30:28 -0700

    Add animations (rot, size, etc.)

--- manim_parser.py
+++ manim_parser.py
@@ -1,3 +1,6 @@
+import math
+
+# TODO: remove after testing
 import os
 import pickle
 import os
@@ -14,23 +17,39 @@ def generate_manim_file(fname, anim_data):
     # TODO: test should be replaced with tmp
     fpath = f"test/{fname}/{fname}.py"
 
+    # create objects
     for shape_i, shape in enumerate(anim_data): 
         shape_type = shape["typeStr"]
         states = shape["states"]
 
-        # handle animations
+        for state_i, state in enumerate(states): 
+            # make the obj 
+            if shape_type == "Circle": 
+                fcontents += f"{dtab}obj_{shape_i}_{state_i} = {construct_circle_str(state)} \n"
+            elif shape_type == "Square": 
+                fcontents += f"{dtab}obj_{shape_i}_{state_i} = {construct_square_str(state)} \n"
+
+            # set state
+            fcontents += f"{dtab}obj_{shape_i}_{state_i}.set_x({state['x']}) \n"
+            fcontents += f"{dtab}obj_{shape_i}_{state_i}.set_y({state['y']}) \n"
+            fcontents += f"{dtab}obj_{shape_i}_{state_i}.scale({state['size']}) \n"
+            fcontents += f"{dtab}obj_{shape_i}_{state_i}.rotate({state['rot']*math.pi/180}, about_point=[{state['x']}, {state['y']}, 0] ) \n"
+
+    # create animations
+    for shape_i, shape in enumerate(anim_data): 
+        states = shape["states"]
         for state_i, state in enumerate(states): 
             # init the mobject
             if state_i == 0: 
-                if shape_type == "Circle": 
-                    fcontents += f"{dtab}obj{shape_i} = {construct_circle_str(state)} \n"
-                elif shape_type == "Square": 
-                    fcontents += f"{dtab}obj{shape_i} = {construct_square_str(state)} \n"
+                fcontents += f"{dtab}self.add(obj_{shape_i}_{state_i})\n"
+            # transform from prev obj to curr obj
+            else: 
+                run_time = state["time"] - states[state_i-1]["time"]
+                run_time = 0 if run_time < 0 else run_time
+                fcontents += f"{dtab}self.play(Transform( obj_{shape_i}_{state_i-1}, obj_{shape_i}_{state_i}, run_time={run_time} )) \n"
 
-
-        fcontents += f"{dtab}self.add(obj{shape_i})\n"
-        fcontents += f"{dtab}self.wait(1)\n"
-    
+    # end vid with 1 sec wait
+    fcontents += f"{dtab}self.wait(1)\n"
 
     with open(fpath, "w") as f: 
         f.write(fcontents)
@@ -38,11 +57,10 @@ def generate_manim_file(fname, anim_data):
 
 
 def construct_circle_str(state): 
-    return f"Circle(radius={state['size']}, color=\"{state['color']}\")"
+    return f"Circle(radius=1, color=\"{state['color']}\")"
     
 def construct_square_str(state): 
     return f"Square(color=\"{state['color']}\")"
-
 
 
 
@@ -60,6 +78,22 @@ def test_gen():
 
     with open("test/hello/anim_data.p", 'rb') as f: 
         anim_data = pickle.load(f)
+    
+    anim_data[0]["states"][0]["x"] = 0
+    anim_data[0]["states"][0]["y"] = 0
+    anim_data[0]["states"][0]["time"] = 0
+    anim_data[0]["states"][1]["x"] = 2
+    anim_data[0]["states"][1]["y"] = 1
+    anim_data[0]["states"][1]["size"] = 2
+    anim_data[0]["states"][1]["time"] = 1
+
+    anim_data[1]["states"][0]["x"] = 4
+    anim_data[1]["states"][0]["y"] = 0
+    anim_data[1]["states"][0]["time"] = 0
+    anim_data[1]["states"][1]["x"] = 1
+    anim_data[1]["states"][1]["y"] = 0
+    anim_data[1]["states"][1]["time"] = 2
+    anim_data[1]["states"][1]["rot"] = 45 
 
     generate_manim_file(fname, anim_data)
 
