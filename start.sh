2f015b0f292353ead9d513e2d77fe62d0dbeaa6a
Austin Totty <awtotty@gmail.com>
2021-07-29 16:22:46 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-gunicorn --certfile cert.pem --keyfile key.pem -b 0.0.0.0:5000 -w 8 app:app
+gunicorn --certfile /etc/letsencrypt/live/www.maniminteractiveserver.com/fullchain.pem --keyfile /etc/letsencrypt/live/www.maniminteractiveserver.com/privkey.pem -b 0.0.0.0:5000 -w 8 app:app
